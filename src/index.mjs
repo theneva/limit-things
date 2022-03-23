02ff5a292c5354cd4fb185f0080061b499ffad67
Martin Lehmann <martin@folio.no>
2022-03-23 15:01:05 +0100

    --wip-- [skip ci]

--- src/index.mjs
+++ src/index.mjs
@@ -1,24 +1,4 @@
-import limit from 'p-limit';
-import { log } from './logger.mjs';
-import { predict } from './predictor.mjs';
-
-const oneAtATime = limit(1);
-
-async function requestPrediction(id) {
-  // Start timing
-  const startTime = new Date();
-
-  // Log that we started
-  log(`Requesting prediction for ${id}`);
-
-  // Wait for queue & prediction (passing args on to getPrediction(…))
-  const prediction = await oneAtATime(() => predict(id));
-
-  // Log some info
-  log(`Got prediction ${prediction}\t(waited ${Date.now() - startTime} ms in total)`);
-
-  return prediction;
-}
+import { requestPrediction } from './prediction-requester.mjs';
 
 // Make 4 predictions at once
 requestPrediction('liam');
---
+++ src/prediction-requester.mjs
@@ -0,0 +1,21 @@
+import limit from 'p-limit';
+import { log } from './logger.mjs';
+import { predict } from './predictor.mjs';
+
+const oneAtATime = limit(1);
+
+export async function requestPrediction(id) {
+  // Start timing
+  const startTime = new Date();
+
+  // Log that we started
+  log(`Requesting prediction for ${id}`);
+
+  // Wait for queue & prediction (passing args on to getPrediction(…))
+  const prediction = await oneAtATime(() => predict(id));
+
+  // Log some info
+  log(`Got prediction ${prediction}\t(waited ${Date.now() - startTime} ms in total)`);
+
+  return prediction;
+}
